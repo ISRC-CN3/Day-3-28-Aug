cd23e2e958b55964d8f63774a01f8ae9c3de3c5a
abdwizard <114934494+abdwizard@users.noreply.github.com>
2022-10-25 21:14:54 +0100

    Delete Lab/Girijesh Prasad directory

--- Lab/Girijesh Prasad/MEGBCI2020-master/functions/crossvalidation_SVM.m
+++
@@ -1,9 +0,0 @@
-function indices = crossvalidation_SVM(y, nfold)
-    %# stratified n-fold cros-validation
-    %#indices = crossvalind('Kfold', y, nfold);  %# Bioinformatics toolbox
-    cv = cvpartition(y, 'kfold',nfold);          %# Statistics toolbox
-    indices = zeros(size(y));
-    for i=1:nfold
-        indices(cv.test(i)) = i;
-    end
-end
